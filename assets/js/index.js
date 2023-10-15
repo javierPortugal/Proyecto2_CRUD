3ca4ba6389b0f9431d9b91a763199abb10b11d98
Javier Portugal <javier.s.portugal@gmail.com>
2023-10-14 22:56:13 -0600

    update index.html
    se agregan imagenes a assets e inicia javascrip

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,23 @@
 console.log("Entro index.js");
+
+
+//referencia a los inputs
+//************************* */
+
+const inputTitulo = document.getElementById("inputTitulo");
+const inputEdicion = document.getElementById("inputEdicion");
+const inputArea = document.getElementById("inputArea");
+const inputImagen = document.getElementById("inputImagen");
+const inputResumen = document.getElementById("inputResumen");
+
+//Refrencia a los botones
+//************************** */
+
+const btnAgregar = document.getElementById("btnAgregar");
+const btnBorrarTodo = document.getElementById("btnBorrarTodo");
+
+const divLibros = document.getElementById("divLibros");
+const alertSinLibros = document.getElementById("alertSinLibros");
+
+//******         */
+//******         */
